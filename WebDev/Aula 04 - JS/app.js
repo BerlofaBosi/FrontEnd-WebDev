50ecd6b7e666ae35ab542e6f804693a33fc33235
Arthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-05-06 21:18:47 -0300

    Reorganização/Aula07Web

---
+++ WebDev/Aula 04 - JS/app.js
@@ -0,0 +1,12 @@
+// alert("Hello World!")
+let name = prompt("Qual é o seu nome?", "Insira aqui seu nome")
+let resultado = confirm("Você deseja continuar?")
+
+var defasado // Forma antiga de declarar variavel
+let nome = "Arthur" // Valores dinâmicos
+const apiKey = "yvuyi" // Valor imutavel
+
+console.log(`Olá ${name},tudo bem? ${resultado}`)
+
+console.log(name)
+console.log(resultado)
--- WebDev/Aula 1 - JS/app2.js
+++
@@ -1,5 +0,0 @@
-let nome = prompt("Qual é o seu nome?")
-let idade = prompt("Qual a sua idade?")
-let emprego = prompt("Com o que você trabalha?")
-
-console.log(`Olá ${nome}! Seja bem vindo. Você tem ${idade} e trabalha com ${emprego}.`)
