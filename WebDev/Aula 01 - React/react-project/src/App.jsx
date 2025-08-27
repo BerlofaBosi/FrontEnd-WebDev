7650694da8173c0eb436f0cf588f87a0995f6875
Arthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-08-26 22:35:10 -0300

    Aula 01 React

---
+++ WebDev/Aula 01 - React/react-project/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header 
+        titulo = "Gui Negão"
+        texto = "Texto 1"
+        cor = "black"/>
+      <Header 
+        titulo = "Buldogue Preto"
+        texto = "Texto 2"
+        cor = "red"/>
+      <Header 
+        titulo = "Davi pnc"
+        texto = "Texto 3"
+        cor = "yellow"/>
+    </>
+  )
+}
+
+export default App
